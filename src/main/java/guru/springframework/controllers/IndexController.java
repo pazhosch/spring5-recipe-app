c703456eb9c9879e44e5920776df31dcd6cfe611
pazhosch <d.m.baranov@gmail.com>
2019-06-16 22:55:58 +0300

    assignment

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -3,20 +3,25 @@ package guru.springframework.controllers;
 import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Controller
 public class IndexController {
 
     private CategoryRepository categoryRepository;
     private UnitOfMeasureRepository unitOfMeasureRepository;
+    private RecipeRepository recipeRepository;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository, RecipeRepository recipeRepository) {
         this.categoryRepository = categoryRepository;
         this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.recipeRepository = recipeRepository;
     }
 
     @RequestMapping({"","/","index"})
@@ -25,6 +30,14 @@ public class IndexController {
         Optional<UnitOfMeasure> unitOfMeasuerOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
         System.out.println("Cat: "+categoryOptional.get( ).getId());
         System.out.println("Unit: "+unitOfMeasuerOptional.get().getId());
+
+        model.addAttribute("recipes",recipeRepository.findAll());
         return "index";
+    }
+
+    @RequestMapping("/recipe/show/*")
+    public String getRecipe(Model model) {
+        model.addAttribute("recipe",recipeRepository.findById(1L).get());
+        return "recipe";
     }
 }
